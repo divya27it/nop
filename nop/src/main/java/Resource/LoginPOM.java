b2358cbf68e2a47c86e5d91a6e1ca7aa3e289119
divya27it <141820168+divya27it@users.noreply.github.com>
2023-11-25 16:08:05 +0530

    Update LoginPOM.java

--- nop/src/main/java/Resource/LoginPOM.java
+++ nop/src/main/java/Resource/LoginPOM.java
@@ -1,19 +1,1 @@
-package Resource;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
-public class LoginPOM {
-
-
-    public void Login(WebDriver driver,String Email,String pwd){
-        driver.findElement(By.id("Email")).sendKeys(Email);
-        driver.findElement(By.id("Password")).sendKeys(pwd);
-        driver.findElement(By.xpath("//*[text()='Log in']")).click();
-    }
-    public void verifydashboard(WebDriver driver){
-       WebElement dashboard=driver.findElement(By.xpath("//*[text()=' Dashboard']"));
-       dashboard.isDisplayed();
-    }
-}
